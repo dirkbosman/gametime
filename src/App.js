3732bbfb1ff0f20ebd500828667ae9a37a1acbaf
Dirk Bosman <dirkjobosman@gmail.com>
2020-08-16 22:14:49 +0200

    rich text and dotenv for keys

--- src/App.js
+++ src/App.js
@@ -1,9 +1,67 @@
-import React from 'react';
-import logo from "./assets/wikigames-logo.png"
-import './App.css';
+import React, { useState, useEffect } from "react";
+import * as Contentful from "contentful";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS, INLINES, MARKS } from "@contentful/rich-text-types";
 
+import logo from "./assets/wikigames-logo.png";
+import "./App.css";
+
+require("dotenv").config();
+
+const client = Contentful.createClient({
+  space: process.env.REACT_APP_SPACE_ID,
+  accessToken: process.env.REACT_APP_CDAPI_ACCESS_TOKEN,
+});
+
+const options = {
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (node, children) => (
+      <p className="paragraph_test">{children}</p>
+    ),
+    [INLINES.HYPERLINK]: (node, children) => (
+      <a href={node.data.uri}>{children}</a>
+    ),
+    // [BLOCKS.EMBEDDED_ENTRY]: (node, children) => <div>{children}</div>,
+    [BLOCKS.EMBEDDED_ASSET]: (node) => (
+      <img
+        className="image"
+        src={node.data.target.fields.file.url}
+        alt={node.data.target.fields.file.url}
+      />
+    ),
+  },
+  renderMark: {
+    [MARKS.ITALIC]: (text) => <span className="italic">{text}</span>,
+  },
+};
 
 function App() {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    client
+      // rename games to contentType
+      .getEntries({ content_type: "blogText" })
+      .then((response) => {
+        setEntries(response.items);
+        // setEntries(..., response.items);
+      });
+  }, []);
+
+  const Entries = entries.map((entry) => (
+    <div className="entry" key={entry.sys.id}>
+      <h1>{entry.fields.title}</h1>
+      {documentToReactComponents(entry.fields.content, options)}
+      {/* {entry.fields.content} */}
+      {/* {documentToReactComponents(entry.fields.content, options)}
+      {entry.fields.category}
+      {entry.fields.subCategory}
+      {entry.fields.image}
+      {entry.fields.players}
+      {entry.fields.description} */}
+    </div>
+  ));
+
   return (
     <div className="App">
       <header>
@@ -15,7 +73,7 @@ function App() {
         </div>
       </header>
       <main>
-        <div className="mainWrapper"></div>
+        <div className="mainWrapper">{Entries}</div>
       </main>
       <footer>
         <div className="footerWrapper"></div>
