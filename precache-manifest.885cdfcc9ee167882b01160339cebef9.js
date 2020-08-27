a26c1a25710a1c131b1374291ab3bade3f18d308
Dirk Bosman <dirkjobosman@gmail.com>
2020-08-27 23:49:35 +0200

    Updates

---
+++ precache-manifest.885cdfcc9ee167882b01160339cebef9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bec5bc37ef08d3aa1410f38ad66d32",
+    "url": "/nomoreboredom/index.html"
+  },
+  {
+    "revision": "873fe61e0d2679d0bce7",
+    "url": "/nomoreboredom/static/css/main.c15f133a.chunk.css"
+  },
+  {
+    "revision": "e21c4cbd5eeecb17e623",
+    "url": "/nomoreboredom/static/js/2.94287d1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nomoreboredom/static/js/2.94287d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "873fe61e0d2679d0bce7",
+    "url": "/nomoreboredom/static/js/main.b1fdea51.chunk.js"
+  },
+  {
+    "revision": "4a04f2ec26d63a3d3cf6",
+    "url": "/nomoreboredom/static/js/runtime-main.44be6b94.js"
+  },
+  {
+    "revision": "fb6e01d632d0e887fd26504d032937e8",
+    "url": "/nomoreboredom/static/media/logo1.fb6e01d6.png"
+  },
+  {
+    "revision": "a7b085105445dbf1357d87c5e6facb95",
+    "url": "/nomoreboredom/static/media/logo11.a7b08510.png"
+  },
+  {
+    "revision": "6f60e42b5d649d8f64f7733e2e49994f",
+    "url": "/nomoreboredom/static/media/logo12.6f60e42b.png"
+  },
+  {
+    "revision": "3987dec057877f0214bdca27ff50a025",
+    "url": "/nomoreboredom/static/media/logo13.3987dec0.png"
+  },
+  {
+    "revision": "189a57f12048d342f9b9571fc19b4a26",
+    "url": "/nomoreboredom/static/media/logo14.189a57f1.png"
+  },
+  {
+    "revision": "82411ab232a3bb5f66168725796080c7",
+    "url": "/nomoreboredom/static/media/logo15.82411ab2.png"
+  },
+  {
+    "revision": "a7b085105445dbf1357d87c5e6facb95",
+    "url": "/nomoreboredom/static/media/logo16.a7b08510.png"
+  },
+  {
+    "revision": "6d6afb7a6784fb4c1b37334cec9ddee1",
+    "url": "/nomoreboredom/static/media/logo17.6d6afb7a.png"
+  },
+  {
+    "revision": "801d601d749f4fc7b71b3ef8894e1e46",
+    "url": "/nomoreboredom/static/media/logo19.801d601d.png"
+  },
+  {
+    "revision": "760e61e68d439fd903eed3aff038b25b",
+    "url": "/nomoreboredom/static/media/logo2.760e61e6.png"
+  },
+  {
+    "revision": "8a77d38633acd7e5fdce55db04390bfe",
+    "url": "/nomoreboredom/static/media/logo20.8a77d386.png"
+  },
+  {
+    "revision": "621271c0b9a41756a8f5c3c311a39e73",
+    "url": "/nomoreboredom/static/media/logo3.621271c0.png"
+  },
+  {
+    "revision": "4c567b084fbb42647afce7591c00fab0",
+    "url": "/nomoreboredom/static/media/logo4.4c567b08.png"
+  },
+  {
+    "revision": "8a06683f6b05fd3101144415d3308bf5",
+    "url": "/nomoreboredom/static/media/logo5.8a06683f.png"
+  },
+  {
+    "revision": "c82338f450e44105747c4edee2fef9cb",
+    "url": "/nomoreboredom/static/media/logo6.c82338f4.png"
+  },
+  {
+    "revision": "d516fd4d05c120dd3642b32d7c5b31dd",
+    "url": "/nomoreboredom/static/media/logo7.d516fd4d.png"
+  },
+  {
+    "revision": "a8e631121190e945d562ded6999afa19",
+    "url": "/nomoreboredom/static/media/logo9.a8e63112.png"
+  }
+]);
