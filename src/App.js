56d6c87e960df394732f4f8284c2ba98059fcae0
Dirk Bosman <dirkjobosman@gmail.com>
2020-09-01 20:09:58 +0200

    hashrouter update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
-import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+// import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch, HashRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import PreviewWrapper from "./components/PreviewWrapper/PreviewWrapper";
 import Entry from "./components/Entry/Entry";
